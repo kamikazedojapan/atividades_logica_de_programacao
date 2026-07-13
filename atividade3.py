1fab5e8df2b0a10053b0ffdfbbdf0f2027af8c39
Dev Márcio <63491248+kamikazedojapan@users.noreply.github.com>
2026-07-13 18:30:03 -0300

    refactor: correção na lógica do código

--- atividade3.py
+++ atividade3.py
@@ -1,22 +1,24 @@
 lista_de_pessoas = []
 c = 0
 
-while c < 3:
-    nome_valido = False
-    while nome_valido == False:
-        nome = input("Nome: ")
-        nome_sem_espaco = nome.replace(' ', '')
-        if nome_sem_espaco != '' and nome_sem_espaco.isalpha():
-            confirmacao_valida = False
-            while confirmacao_valida == False:
-                confirmacao = input("Você deseja participar da seleção de estágio? (S/N)\n")
-                if confirmacao == 's' or confirmacao == 'S':
-                    lista_de_pessoas.append(nome)
-                    confirmacao_valida = True
-                    c += 1
-                elif confirmacao == 'n' or confirmacao == 'N':
-                    confirmacao_valida = True
-                else:
-                    print("Digite apenas S ou N para confirmar.")
-        else:
-            print("Nome inválido. Tente novamente")
+while True:
+  nome = input("Nome: ")
+  nome_sem_espaco = nome.replace(' ', '')
+  if nome_sem_espaco != '' and nome_sem_espaco.isalpha():
+      while True:
+          confirmacao = input("Você deseja participar da seleção de estágio? (S/N)\n")
+          if confirmacao == 's' or confirmacao == 'S':
+              lista_de_pessoas.append(nome)
+              c += 1
+              break
+          elif confirmacao == 'n' or confirmacao == 'N':
+              break
+          else:
+              print("Digite apenas S ou N para confirmar.")
+          break
+  else:
+      print("Nome inválido. Tente novamente")
+  if c == 3:
+    print(f"Pessoas na lista: {c}")
+    print(f"Lista de pessoas: {lista_de_pessoas}")
+    break
