4f8ff95f1016856824c3aece1896b285b2ef1be5
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-07-16 10:26:46 -0300

    feat: Cria uma calculadora utilizando funções

---
+++ calculadora.py
@@ -0,0 +1,65 @@
+c  = True
+
+def menu_opcoes():
+    print("======= Calculadora =======")
+    print("1 - Soma")
+    print("2 - Diferença")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Sair")
+
+def soma():
+    n1 = float(input("1° Número: "))
+    n2 = float(input("2° Número: "))
+    soma = n1 + n2
+    print(f"\nSoma: {soma}")
+    return soma
+
+def diferenca():
+    n1 = float(input("1° Número: "))
+    n2 = float(input("2° Número: "))
+    diferenca = n1 - n2
+    print(f"\nDiferença: {diferenca}")
+    return diferenca
+
+def multiplicacao():
+    n1 = float(input("1° Número: "))
+    n2 = float(input("2° Número: "))
+    multiplicacao = n1 * n2
+    print(f"\nMultiplicação: {multiplicacao}")
+    return multiplicacao
+
+def divisao():
+    n1 = float(input("1° Número: "))
+    n2 = float(input("2° Número: "))
+    divisao = n1 / n2
+    print(f"\nDivisão: {divisao}")
+    return divisao
+
+def confirmacao():
+    confirmacao = input("Deseja continuar (s/n): ").lower()
+    if confirmacao != "s" and confirmacao != 'n':
+        print("Opção inválida.")
+    elif confirmacao == 's':
+        return
+    else:
+        c = False
+
+
+while c == True:
+    menu_opcoes()
+    opcao = input("\nDigite uma opção: ")
+    if opcao == '1':
+        soma()
+        confirmacao()
+    elif opcao == '2':
+        diferenca()
+        confirmacao()
+    elif opcao == '3':
+        multiplicacao()
+        confirmacao()
+    elif opcao == '4':
+        divisao()
+        confirmacao()
+    elif opcao == '5':
+        exit()
